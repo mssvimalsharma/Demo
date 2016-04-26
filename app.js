3588c7ce0f4547ac0bb017db8d216a45a7856d31
Vimal Sharma <mss.vimalsharma@gmail.com>
2016-04-26 17:04:42 +0530

    add route

--- app.js
+++ app.js
@@ -6,6 +6,10 @@ app.get('/', function (req, res) {
   res.send('Hello World!');
 });
 
+app.get('/webhook', function (req, res) {
+  res.send('Hello World!');
+});
+
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
 });
